05e1b3116a226ef4d1361d685785f7b7cd202639
Jayaditya Dev <jayadityadev26@outlook.com>
2023-09-04 20:26:03 +0530

    Minor fixes: separate function created for welcome script

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ def tts_engine(text):
     engine.runAndWait()
 
 
-welcome_string = '\nWelcome to RoboSpeaker 1.0. Created by Jayaditya Dev.'
-print(welcome_string)
-tts_engine(welcome_string)
-print("[Type 'qq' to exit.]\n")
+def welcome():
+    welcome_string = '\nWelcome to RoboSpeaker 1.0. Created by Jayaditya Dev.'
+    print(welcome_string)
+    tts_engine(welcome_string)
+    print("[Type 'qq' to exit.]\n")
 
 
 if __name__ == '__main__':
+    welcome()
     while True:
         text = input("Enter what you want to say: ")
         if text == 'qq':
